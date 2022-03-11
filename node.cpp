c365c92903a8fab629f7426fe9531f4bd2e2cf4e
MoKholy <moelkholy@aucegypt.edu>
2022-03-11 19:29:45 +0200

    node class and header files
    Initial members and methods for the class, to be modified later

---
+++ node.cpp
@@ -0,0 +1,130 @@
+#include "node.hpp"
+using namespace std;
+
+Node::Node()
+{
+
+}
+
+Node::Node(string paper_title, int num_auth, vector<string> auth_names, int num_citations, vector<string> cit_titles, int num_references, vector<string> ref_titles, int year, string journal_name, string paper_doi)
+{
+
+}
+
+// getters
+string Node::get_title()
+{
+
+}
+
+int Node::get_num_authors()
+{
+
+}
+
+vector<string> Node::get_author_names()
+{
+
+}
+
+int Node::get_num_citations()
+{
+
+}
+
+vector<string> Node::get_citation_titles()
+{
+
+}
+
+int Node::get_num_references()
+{
+
+}
+
+vector<string> Node::get_reference_titles()
+{
+
+}
+
+int Node::get_publish_year()
+{
+
+}
+
+string Node::get_journal_name()
+{
+
+}
+
+string Node::get_paper_doi()
+{
+
+}
+
+// setters
+void Node::set_title(string paper_title)
+{
+
+}
+
+void Node::set_num_authors(int num_auth)
+{
+
+}
+
+void Node::set_author_names(vector<string> auth_names)
+{
+
+}
+
+void Node::set_num_citations(int num_citations)
+{
+
+}
+
+void Node::set_citation_titles(vector<string> cit_titles)
+{
+
+}
+
+void Node::set_num_references(int num_references)
+{
+
+}
+
+void Node::set_reference_titles(vector<string> ref_titles)
+{
+
+}
+
+void Node::set_publish_year(int year)
+{
+
+}
+
+void Node::set_journal_name(string journal_name)
+{
+
+}
+
+void Node::set_paper_doi(string paper_doi)
+{
+    
+}
+
+// debugging 
+void Node::print() // prints details of node
+{
+
+} 
+
+// destructor
+Node::~Node()
+{
+
+}
+
+int main(){
+    return 0;
+}
---
+++ node.hpp
@@ -0,0 +1,61 @@
+#ifndef NODE_H
+#define NODE_h
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Node{
+
+    private:
+
+    string title; // title of paper
+    int n_authors; // number of authors 
+    vector<string> authors; // list of author last names
+    int n_citations; // number of citations paper received
+    vector<string> citation_titles; // titles of papers that cited this paper
+    int n_references; // number of references used in this paper
+    vector<string> references_titles; // titles of papers referenced
+    int year_published; // year paper was published
+    string journal_name; // name of journal paper was published in
+    string doi; // doi of research paper 
+
+    public:
+    //default constructor
+    Node();
+    // parametarized consructor
+    Node(string paper_title, int num_auth, vector<string> auth_names, int num_citations, vector<string> cit_titles, int num_references, vector<string> ref_titles, int year, string journal_name, string paper_doi);
+
+    // getters
+    string get_title();
+    int get_num_authors();
+    vector<string> get_author_names();
+    int get_num_citations();
+    vector<string> get_citation_titles();
+    int get_num_references();
+    vector<string> get_reference_titles();
+    int get_publish_year();
+    string get_journal_name();
+    string get_paper_doi();
+
+    // setters
+    void set_title(string paper_title);
+    void set_num_authors(int num_auth);
+    void set_author_names(vector<string> auth_names);
+    void set_num_citations(int num_citations);
+    void set_citation_titles(vector<string> cit_titles);
+    void set_num_references(int num_references);
+    void set_reference_titles(vector<string> ref_titles);
+    void set_publish_year(int year);
+    void set_journal_name(string journal_name);
+    void set_paper_doi(string paper_doi);
+
+    // debugging 
+    void print(); // prints details of node
+
+    // destructor
+    ~Node();
+};
+
+#endif
